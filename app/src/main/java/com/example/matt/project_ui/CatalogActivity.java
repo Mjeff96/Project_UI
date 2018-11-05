e99a0750b704b832e9888140d51adcb98677e186
Matthew Jeffers <mjeffers96@gmail.com>
2018-11-04 19:34:15 -0500

    Added catalog selection functionality. So the user goes to the correct catalog they selected using the drop down menu.

--- app/src/main/java/com/example/matt/project_ui/CatalogActivity.java
+++ app/src/main/java/com/example/matt/project_ui/CatalogActivity.java
@@ -10,14 +10,17 @@ import android.widget.Spinner;
 
 public class CatalogActivity extends AppCompatActivity {
 
+
+    Spinner dropdown;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
-        Spinner dropdown = findViewById(R.id.spinner);
+
         String[] items = new String[]{"Arms", "Legs", "Yoga"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown = findViewById(R.id.spinner);
         dropdown.setAdapter(adapter);
 
         Button button = (Button) findViewById(R.id.button2);
@@ -31,7 +34,17 @@ public class CatalogActivity extends AppCompatActivity {
     }
 
     public void next_page(View v) {
-        Intent intent = new Intent(this, ArmsActivity.class);
+        String text = dropdown.getSelectedItem().toString();
+        Intent intent;
+
+        if(text == "Arms"){
+            intent = new Intent(this, ArmsActivity.class);
+        }else if(text == "Legs"){
+            intent = new Intent(this, LegsActivity.class);
+        }else{
+            intent = new Intent(this, YogaActivity.class);
+        }
+
         startActivity(intent);
     }
 }
